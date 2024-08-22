e216f73437db25a7e5ae9cc132696a97f113b31d
aryaHarshwardhan <santozstudios@gmail.com>
2024-08-22 18:40:49 +0530

    Fixed styling
    Added vs code setting to format code on save

--- src/shared/Modules/Constants.lua
+++ src/shared/Modules/Constants.lua
@@ -1,13 +1,13 @@
 local Constants = {
-    GAME_STATES = {
-        ROUND_STARTED = "RoundStarted",
-        RATING = "Rating",
-        RESULTS = "Results",
-        INTERMISSION = "Intermission",
-    },
-    EVENTS = {
-        GAME_STATE_EVENTS = "GameStateEvents",
-    }
+	GAME_STATES = {
+		ROUND_STARTED = "RoundStarted",
+		RATING = "Rating",
+		RESULTS = "Results",
+		INTERMISSION = "Intermission",
+	},
+	EVENTS = {
+		GAME_STATE_EVENTS = "GameStateEvents",
+	},
 }
 
 return Constants
