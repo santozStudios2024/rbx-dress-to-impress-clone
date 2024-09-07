1ad51e066142e0d7c2a46d03de87b4cba2df16fe
aryaHarshwardhan <santozstudios@gmail.com>
2024-09-07 17:36:19 +0530

    Added Avatar VP Component

---
+++ Packages/viewportmodel.lua
@@ -0,0 +1,1 @@
+return require(script.Parent._Index["bubshurb_viewportmodel@1.0.0"]["viewportmodel"])
--- src/client/Modules/MainHud/Menus/BodyCustomizationScreen/init.lua
+++ src/client/Modules/MainHud/Menus/BodyCustomizationScreen/init.lua
@@ -1,12 +1,16 @@
+-- Services --
+local Players = game:GetService("Players")
+
 -- Dpendencies --
 local ClientModules = script.Parent.Parent.Parent
 local Roact = require(game.ReplicatedStorage.Packages.roact)
 local BaseTheme = require(ClientModules.BaseTheme)
 local TweeningFrame = require(ClientModules.Components.TweeningFrame)
 local ColorPicker = require(ClientModules.Components.ColorPicker)
+local AvatarVpComponent = require(ClientModules.Components.AvatarVpComponent)
 
 -- Variables --
-
+local localPlayer = Players.LocalPlayer
 local createElement = Roact.createElement
 
 local BodyCustomizationScreen = Roact.Component:extend("BodyCustomizationScreen")
@@ -19,7 +23,7 @@ function BodyCustomizationScreen:render()
 			return createElement(TweeningFrame, {
 				BackgroundTransparency = 1,
 				theme = theme,
-				Visible = false,
+				Visible = true,
 			}, {
 				ColorPickerBg = createElement("Frame", {
 					AnchorPoint = theme.ap.right_center,
@@ -28,6 +32,24 @@ function BodyCustomizationScreen:render()
 					BackgroundColor3 = Color3.new(0, 0, 0),
 				}, {
 					ColorPicker = createElement(ColorPicker),
+				}),
+				PlayerAvatarBg = createElement("Frame", {
+					AnchorPoint = theme.ap.left_center,
+					Position = UDim2.fromScale(0.05, 0.5),
+					Size = UDim2.fromScale(0.4, 0.55),
+					BackgroundColor3 = Color3.new(0, 0, 0),
+				}, {
+					Avatar = createElement(AvatarVpComponent, {
+						userId = localPlayer.UserId,
+						canAnimate = true,
+						canRotate = true,
+						canManuallyRotate = true,
+						AnchorPoint = theme.ap.center,
+						Position = theme.pos.center,
+						Size = theme.size,
+						productsInfo = {},
+						Visible = true,
+					}),
 				}),
 			})
 		end,
